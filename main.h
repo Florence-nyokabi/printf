29d89a64d4ec5b80404ec188048d2e7c42d69404
Florence-wangui <wanguif018@gmail.com>
2022-04-19 01:27:22 -0700

    Flo updates

--- main.h
+++ main.h
@@ -1,7 +1,25 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdarg.h>
+
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ *
+ */
+
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
 int putchar (char c);
 int _printf(const char *format, ...);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_R(va_list R);
 
 #endif
