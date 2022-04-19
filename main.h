d6eca40946be383badb8a1f47ec2710c04e7620c
Stephen Muchendu <muchendumaina66@gmail.com>
2022-04-19 18:09:25 +0300

    More Commits

--- main.h
+++ main.h
@@ -1,7 +1,29 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-int putchar (char c);
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+
 int _printf(const char *format, ...);
+int _putchar(char c);
+int print_str(va_list str_list);
+int print_char(va_list char_list);
+int percentage(__attribute__((unused))va_list param);
+int (*converter(char s))(va_list);
 
-#endif
+/**
+ * struct format - structure
+ * @F: char holding the format
+ * @T: pointer function
+ */
+
+typedef struct format
+{
+	char F;
+	int (*T)(va_list);
+} fmt;
+
+#endif /* MAIN_H */
